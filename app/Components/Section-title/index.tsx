8f8ad816254b98d6d8cf88e78d4031ae742cc56e
Luiz Pedro Galdino Silva <luispedrogaldino53@gmail.com>
2024-08-26 15:09:55 -0300

    Formulário de Contato e Footer

--- app/Components/Section-title/index.tsx
+++ app/Components/Section-title/index.tsx
@@ -16,7 +16,7 @@ export const SectionTitle = ({
       <span className='font-mono text-sm text-emerald-400'>
         {`../${subtitle}`}
       </span>
-      <h3 className='text-3xl font-medium'></h3>
+      <h3 className='text-3xl font-medium'>{title}</h3>
     </div>
   );
 };
